1f80e5f4c256e258e5d4d857b3639a37d37ea523
Ivan Liang <ivanwenliang@gmail.com>
2019-10-29 16:55:19 -0700

    Update api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
 import { UserProvider, UserConsumer } from "./UserContext";
+import { EmailProvider, EmailConsumer } from "./EmailContext";
 import "./index.css";
 
 const Root = () => {
@@ -15,7 +16,9 @@ const Root = () => {
 
 ReactDOM.render(
   <UserProvider>
-    <Root />
+    <EmailProvider>
+      <Root />
+    </EmailProvider>
   </UserProvider>,
   document.querySelector("#root")
 );
