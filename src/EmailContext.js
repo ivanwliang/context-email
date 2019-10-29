d92a2a65a3e7907315462b1abdb468be76f3cf43
Ivan Liang <ivanwenliang@gmail.com>
2019-10-29 16:53:46 -0700

    Create new email context

---
+++ src/EmailContext.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { fetchEmails } from "./api";
+
+const { Provider, Consumer } = React.createContext();
+
+class EmailProvider extends React.Component {
+  state = {
+    emails: [],
+    currentEmail: null,
+    error: null,
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true, error: null });
+    fetchEmails()
+      .then(emails => this.setState({ loading: false, emails }))
+      .catch(error => this.setState({ loading: false, error }));
+  }
+
+  handleSelectEmails = email => {
+    this.setState({ currentEmail: email });
+  };
+
+  render() {
+    return (
+      <Provider
+        value={{ ...this.state, onSelectEmail: this.handleSelectEmails }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export { EmailProvider, Consumer as EmailConsumer };
