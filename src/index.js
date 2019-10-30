520b0d43a125329bd2f9cd1d0a6b144aa2315d11
Ivan Liang <ivanwenliang@gmail.com>
2019-10-29 18:54:16 -0700

    Wrap root app with NotificationProvider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
 import { UserProvider, UserConsumer } from "./UserContext";
+import { NotificationProvider } from "./NotificationContext";
 import { EmailProvider } from "./EmailContext";
 import "./index.css";
 
@@ -15,10 +16,12 @@ const Root = () => {
 };
 
 ReactDOM.render(
-  <UserProvider>
-    <EmailProvider>
-      <Root />
-    </EmailProvider>
-  </UserProvider>,
+  <NotificationProvider>
+    <UserProvider>
+      <EmailProvider>
+        <Root />
+      </EmailProvider>
+    </UserProvider>
+  </NotificationProvider>,
   document.querySelector("#root")
 );
